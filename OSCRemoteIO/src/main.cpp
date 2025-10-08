0e802333199a0e8c8cf3157daa63965670248233
TheLadyLuthien <luthienfinch@outlook.com>
2025-10-07 21:38:33 -0700

    it compiles! Horrah!

--- OSCRemoteIO/src/main.cpp
+++ OSCRemoteIO/src/main.cpp
@@ -1,18 +1,40 @@
-#include <Arduino.h>
+#include <Adafruit_NeoPixel.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// How many internal neopixels do we have? some boards have more than one!
+#define NUMPIXELS 1
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+Adafruit_NeoPixel pixels(NUMPIXELS, PIN_NEOPIXEL, NEO_GRB + NEO_KHZ800);
+
+// the setup routine runs once when you press reset:
+void setup()
+{
+    Serial.begin(115200);
+
+#if defined(NEOPIXEL_POWER)
+    // If this board has a power control pin, we must set it to output and high
+    // in order to enable the NeoPixels. We put this in an #if defined so it can
+    // be reused for other boards without compilation errors
+    pinMode(NEOPIXEL_POWER, OUTPUT);
+    digitalWrite(NEOPIXEL_POWER, HIGH);
+#endif
+
+    pixels.begin();           // INITIALIZE NeoPixel strip object (REQUIRED)
+    pixels.setBrightness(20); // not so bright
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+// the loop routine runs over and over again forever:
+void loop()
+{
+    // say hi
+    Serial.println("Hello!");
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    // set color to red
+    pixels.fill(0xFF0000);
+    pixels.show();
+    delay(500); // wait half a second
+
+    // turn off
+    pixels.fill(0x000000);
+    pixels.show();
+    delay(500); // wait half a second
 }
